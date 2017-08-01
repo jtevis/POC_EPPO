e5e3adf0550960dba6184d54b43481290e034479
jtevis <jwtevis@vis4ag.com>
2017-08-01 14:44:22 -0500

    Separated into four REST GET functions.

--- src/main/java/com/vis/poc_eppo/MainActivity.java
+++ src/main/java/com/vis/poc_eppo/MainActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
     public void onClickNewEPPO(View v) {
 
@@ -19,5 +29,29 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         }
 
+    }
+
+    public void onClickNewWeather(View v) {
+
+        if (v.getId() == R.id.CWeather) {
+            Intent i = new Intent(MainActivity.this, weather_request.class);
+            startActivity(i);
+        }
+
+    }
+    public void onClickNewGitHub(View v) {
+
+        if (v.getId() == R.id.CGitHub) {
+            Intent i = new Intent(MainActivity.this, github_entry.class);
+            startActivity(i);
+        }
+
+    }
+    public void onClickMixMate(View v) {
+
+        if (v.getId() == R.id.CMixMate) {
+            Intent i = new Intent(MainActivity.this, mixmate_request.class);
+            startActivity(i);
+        }
     }
 }
--- src/main/java/com/vis/poc_eppo/eppo_request.java
+++ src/main/java/com/vis/poc_eppo/eppo_request.java
@@ -1,9 +1,17 @@
 package com.vis.poc_eppo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by XPS13 on 7/6/2017.
@@ -11,13 +19,48 @@ import android.view.View;
 
 public class eppo_request extends AppCompatActivity {
 
+
+    //String API_BASE_URL = "https://data.eppo.int/api/rest/1.0/taxon/CORN?authtoken=" + eppo_token;
+    String API_BASE_URL = "https://data.eppo.int/";
+
+    private ListView listview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eppo_request);
 
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create());
+
+        listview = (ListView) findViewById(R.id.list_item_pagination);
+
+
+       Retrofit retrofit = builder.build();
+
+        // Create a very simple REST adapter which points the GitHub API endpoint.
+        EPPOClient client =  retrofit.create(EPPOClient.class);
+
+// Fetch a list of the Github repositories.
+       Call<List<EPPOResponse>> call = client.EPPOForUser("corn");
+
+// Execute the call asynchronously. Get a positive or negative callback.
+
+
+       call.enqueue(new Callback<List<EPPOResponse>>() {
+            @Override
+            public void onResponse(Call<List<EPPOResponse>> call, Response<List<EPPOResponse>> response) {
+                List<EPPOResponse> respo = response.body();
+
+              listview.setAdapter(new EPPOResponseAdapter(eppo_request.this, respo));
+            }
+
+            @Override
+            public void onFailure(Call<List<EPPOResponse>> call, Throwable t) {
+                Toast.makeText(eppo_request.this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
-
-
 }
